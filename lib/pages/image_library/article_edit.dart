0cb231fa636c7085747d8db6b3655049653f4969
高橋 宗弘 <16729672+hidakanoko@users.noreply.github.com>
2023-02-07 22:10:59 +0900

    fix: color title bar

--- lib/pages/image_library/article_edit.dart
+++ lib/pages/image_library/article_edit.dart
@@ -1,5 +1,8 @@
 import 'package:flutter/material.dart';
+import 'package:mtoybox/modules/domain/gateway/category_gateway.dart';
 import 'package:mtoybox/modules/domain/model/article/item.dart';
+import 'package:mtoybox/modules/domain/model/category/catetory.dart';
+import 'package:mtoybox/modules/interface/category_repository.dart';
 
 class ArticleEdit extends StatefulWidget {
   final Item _item;
@@ -12,12 +15,29 @@ class ArticleEdit extends StatefulWidget {
 }
 
 class _ArticleEditState extends State<ArticleEdit> {
+  CategoryGateway _categoryGateway = CategoryRepository.instance();
+
   @override
   Widget build(BuildContext context) {
-    return Scaffold(
-      appBar: AppBar(
-        title: Text(widget._item.name),
-      ),
+    return FutureBuilder<Category?>(
+      builder: (context, snapshot) {
+        return Scaffold(
+          appBar: AppBar(
+            title: Text(widget._item.name),
+            backgroundColor: snapshot.hasData ? snapshot.data!.color : null,
+          ),
+        );
+      },
+      future: _findCategory(),
     );
+  }
+
+  Future<Category?> _findCategory() async {
+    var categoryId = widget._item.categoryId;
+    if (categoryId != null) {
+      return await _categoryGateway.findById(categoryId);
+    } else {
+      return null;
+    }
   }
 }
