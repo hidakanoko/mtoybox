b90bc7d0bb2081e3fae08e5fa27bc7d5ad772f39
高橋 宗弘 <16729672+hidakanoko@users.noreply.github.com>
2024-08-18 14:46:59 +0900

    feat: フィルターモーダル

--- lib/components/common/button/category_filter_button.dart
+++ lib/components/common/button/category_filter_button.dart
@@ -1,6 +1,7 @@
 import 'package:flutter/material.dart';
 
 import 'package:flutter_riverpod/flutter_riverpod.dart';
+import 'package:mtoybox/components/category/category_label.dart';
 import 'package:mtoybox/modules/interface/provider_factory.dart';
 
 @immutable
@@ -9,30 +10,85 @@ class CategoryFilterButton extends ConsumerWidget {
 
   @override
   Widget build(BuildContext context, WidgetRef ref) {
-    var cateogry = ref.watch(categoryFilterProvider);
+    var category = ref.watch(categoryFilterProvider);
 
-    return cateogry != null
+    return category != null
         ? IconButton(
-            onPressed: () => {openSelectCategory()},
+            onPressed: () => {openSelectCategory(context, ref)},
             icon: const Icon(
-              Icons.circle,
-              size: 35,
+              Icons.filter_alt,
+              size: 40,
             ),
-            color: Colors.blue)
+            color: category.color)
         : IconButton(
-            onPressed: () => {openSelectCategory()},
+            onPressed: () => {openSelectCategory(context, ref)},
             icon: const Icon(
-              Icons.square,
-              size: 35,
+              Icons.filter_alt_off,
+              size: 40,
             ),
-            color: Colors.red,
+            color: Colors.grey,
           );
   }
 
-  void openSelectCategory() {}
-  // Future<void> openSelectCategory(BuildContext context) {
-  //   return showDialog(context: context, builder: (BuildContext context) => {
-  //     return SimpleDialog(title: Text("test"),);
-  //   });
-  // }
+  void openSelectCategory(BuildContext context, WidgetRef ref) {
+    var categories = ref.watch(categoryRepositoryProvider);
+    final articles = ref.watch(articleRepositoryProvider);
+    showDialog(
+        context: context,
+        builder: (BuildContext context) {
+          return SimpleDialog(
+            backgroundColor: Colors.white,
+            shape: const RoundedRectangleBorder(),
+            contentPadding: const EdgeInsets.all(0),
+            children: [
+              ElevatedButton(
+                onPressed: () {
+                  ref.read(categoryFilterProvider.notifier).state = null;
+                  Navigator.of(context).pop();
+                },
+                style: ElevatedButton.styleFrom(
+                    padding:
+                        const EdgeInsets.symmetric(vertical: 8, horizontal: 24),
+                    shape: const RoundedRectangleBorder(),
+                    backgroundColor: Colors.white),
+                child: Row(
+                  mainAxisAlignment: MainAxisAlignment.start,
+                  children: [
+                    Container(
+                      color: Colors.white,
+                      width: 30,
+                      height: 30,
+                      margin: const EdgeInsets.all(10),
+                    ),
+                    const Text(
+                      'ぜんぶ',
+                      style: TextStyle(fontSize: 30),
+                    ),
+                  ],
+                ),
+              ),
+              ...categories.asList().map((c) {
+                final usageCount = articles
+                    .where((element) => element.categoryId == c.id)
+                    .length;
+
+                return ElevatedButton(
+                    onPressed: () {
+                      ref.read(categoryFilterProvider.notifier).state = c;
+                      Navigator.of(context).pop();
+                    },
+                    style: ElevatedButton.styleFrom(
+                        padding: const EdgeInsets.symmetric(
+                            vertical: 8, horizontal: 24),
+                        shape: const RoundedRectangleBorder(),
+                        backgroundColor: Colors.white),
+                    child: CategoryLabel(
+                      c,
+                      usageCount: usageCount,
+                    ));
+              }).toList()
+            ],
+          );
+        });
+  }
 }
